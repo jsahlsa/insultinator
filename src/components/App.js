2b4ea8e236afd34fdd8e38bee64439ed53535554
jsahlsa <joe.sahlstrom@gmail.com>
2021-08-20 22:37:41 +0000

    💄 added some styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function App() {
-  return <div>Testing!</div>;
+  return (
+    <div class="app-wrapper">
+      <div className="app-backgrounds green"></div>
+      <div className="app-backgrounds black"></div>
+    </div>
+  );
 }
